dfcbd19d8cdd6ff9227f2bfd127f68e187d9ca5d
shry15harsh <shry.harsh@gmail.com>
2014-06-20 16:43:34 +0530

    only some things remaining

--- js/myjs.js
+++ js/myjs.js
@@ -6,4 +6,27 @@ $(document).ready(function(){
         currentImage = (currentImage + 1)%totalImages + 1;
         $('#home img:nth-child('+currentImage+')').css('opacity','1');
     },5000);
+    $('#top .nav-item a[href^="#"]').on('click',function (e) {
+    		console.log(this);
+			e.preventDefault();
+
+			var target = this.hash;
+			$target = $(target);
+			var pos;
+			var topNavHeight;
+	
+			if(target == '#home'){
+				pos = 0;
+				topNavHeight = 0;
+			}
+			else{
+				pos = $target.offset().top;
+				topNavHeight = parseInt($('#top').css('height'), 10);
+			}
+
+			$('html, body').stop().animate({
+					'scrollTop': pos-topNavHeight
+					}, 1000, 'swing', function () {
+			});
+	});
 });
